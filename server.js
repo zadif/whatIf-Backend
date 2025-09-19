878962c0d6785dcc15d3184c68824f923c7d04d3
Zadif <zadifmustafa93@gmail.com>
2025-09-19 21:38:47 +0500

    Managed likes, made RPC to handle increment and decrement of likes, Also inserted and deleted likes in 'likes' tables

--- server.js
+++ server.js
@@ -40,6 +40,75 @@ app.get("/hello", verifyToken, (req, res) => {
   return res.status(200).json({ message: "Secure path" });
 });
 
+app.post("/like", verifyToken, async (req, res) => {
+  let { postID, action } = req.body;
+
+  const decoded = jwt.decode(req.cookies.access_token);
+  const userId = decoded.sub;
+
+  try {
+    let supabase2 = supabaseWithAuth(req);
+    if (action == "like") {
+      //inserting in likes table
+      let { error: likeError } = await supabase2
+        .from("likes")
+        .insert({ userID: userId, whatifID: postID });
+
+      if (likeError) {
+        if (likeError.code === "23505") {
+          // duplicate (user already liked this post)
+          console.log("User already liked this post, ignoring.");
+          return res.status(200).json({ message: "Already liked" });
+        }
+        console.error(
+          "Error while inserting like in supabase from likes table: ",
+          likeError.message
+        );
+        return res.status(500).json({ message: "Server side error" });
+      }
+      //updating in whatifs table
+      const postIDNum = Number(postID);
+      const { data, error } = await supabase2.rpc("increment_like", {
+        row_id: postIDNum,
+      });
+
+      if (error) {
+        console.error("Error calling increment_like:", error);
+        return res.status(500).json({ message: error.message });
+      }
+    } else if (action == "dislike") {
+      //inserting in likes table
+
+      let { error: likeError } = await supabase2
+        .from("likes")
+        .delete()
+        .eq("userID", userId)
+        .eq("whatifID", postID);
+      if (likeError) {
+        console.error(
+          "Error while deleting like in supabase from likes table: ",
+          likeError.message
+        );
+        return res.status(500).json({ message: "Server side error" });
+      }
+      //updating in whatifs table
+      const postIDNum = Number(postID);
+      const { data, error } = await supabase2.rpc("decrement_like", {
+        row_id: postIDNum,
+      });
+
+      if (error) {
+        console.error("Error calling decrement_like:", error);
+        return res.status(500).json({ message: error.message });
+      }
+    }
+
+    res.sendStatus(200);
+  } catch (err) {
+    console.error("Error in like middleware: ", err);
+  }
+});
+
 app.listen(port, () => {
   console.log("Listening on port ", port);
 });
