aeb64b62eadf3021e163c40cc64a8873cff6976a
Zadif <zadifmustafa93@gmail.com>
2025-09-19 21:48:02 +0500

    Made a RPC in supabase which handles both things at once i.e. Inserting in likes table and updating in whatif table

--- server.js
+++ server.js
@@ -48,59 +48,17 @@ app.post("/like", verifyToken, async (req, res) => {
 
   try {
     let supabase2 = supabaseWithAuth(req);
-    if (action == "like") {
-      //inserting in likes table
-      let { error: likeError } = await supabase2
-        .from("likes")
-        .insert({ userID: userId, whatifID: postID });
 
-      if (likeError) {
-        if (likeError.code === "23505") {
-          // duplicate (user already liked this post)
-          console.log("User already liked this post, ignoring.");
-          return res.status(200).json({ message: "Already liked" });
-        }
-        console.error(
-          "Error while inserting like in supabase from likes table: ",
-          likeError.message
-        );
-        return res.status(500).json({ message: "Server side error" });
-      }
-      //updating in whatifs table
-      const postIDNum = Number(postID);
-      const { data, error } = await supabase2.rpc("increment_like", {
-        row_id: postIDNum,
-      });
+    const postIDNum = Number(postID);
+    const { data, error } = await supabase2.rpc("toggle_like", {
+      p_user: userId,
+      p_post_id: postIDNum,
+      action,
+    });
 
-      if (error) {
-        console.error("Error calling increment_like:", error);
-        return res.status(500).json({ message: error.message });
-      }
-    } else if (action == "dislike") {
-      //inserting in likes table
-
-      let { error: likeError } = await supabase2
-        .from("likes")
-        .delete()
-        .eq("userID", userId)
-        .eq("whatifID", postID);
-      if (likeError) {
-        console.error(
-          "Error while deleting like in supabase from likes table: ",
-          likeError.message
-        );
-        return res.status(500).json({ message: "Server side error" });
-      }
-      //updating in whatifs table
-      const postIDNum = Number(postID);
-      const { data, error } = await supabase2.rpc("decrement_like", {
-        row_id: postIDNum,
-      });
-
-      if (error) {
-        console.error("Error calling decrement_like:", error);
-        return res.status(500).json({ message: error.message });
-      }
+    if (error) {
+      console.error("Error calling increment_like:", error);
+      return res.status(500).json({ message: error.message });
     }
 
     res.sendStatus(200);
